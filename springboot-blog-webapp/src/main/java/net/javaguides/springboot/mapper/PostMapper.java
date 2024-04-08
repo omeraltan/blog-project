6d811a7df7e92fb622e401400c0e3dae0435c2a4
omeraltan <omeraltan66@gmail.com>
2024-04-09 00:20:02 +0300

    Commit All

---
+++ springboot-blog-webapp/src/main/java/net/javaguides/springboot/mapper/PostMapper.java
@@ -0,0 +1,32 @@
+package net.javaguides.springboot.mapper;
+
+import net.javaguides.springboot.dto.PostDto;
+import net.javaguides.springboot.entity.Post;
+
+public class PostMapper {
+    // map Post entity to PostDto
+    public static PostDto mapToPostDto(Post post) {
+        return PostDto.builder()
+            .id(post.getId())
+            .title(post.getTitle())
+            .content(post.getContent())
+            .url(post.getUrl())
+            .shortDescription(post.getShortDescription())
+            .createdOn(post.getCreatedOn())
+            .updatedOn(post.getUpdatedOn())
+            .build();
+    }
+
+    // map PostDto to Post entity
+    public static Post mapToPost(PostDto postDto) {
+        return Post.builder()
+            .id(postDto.getId())
+            .title(postDto.getTitle())
+            .content(postDto.getContent())
+            .url(postDto.getUrl())
+            .shortDescription(postDto.getShortDescription())
+            .createdOn(postDto.getCreatedOn())
+            .updatedOn(postDto.getUpdatedOn())
+            .build();
+    }
+}
