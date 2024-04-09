4af572cc95be9d9c21358b3318ddcb1eab1e5416
omeraltan <omeraltan66@gmail.com>
2024-04-09 21:48:45 +0300

    Commit All

--- springboot-blog-webapp/src/main/java/net/javaguides/springboot/controller/PostController.java
+++ springboot-blog-webapp/src/main/java/net/javaguides/springboot/controller/PostController.java
@@ -1,11 +1,15 @@
 package net.javaguides.springboot.controller;
 
+import jakarta.validation.Valid;
 import net.javaguides.springboot.dto.PostDto;
 import net.javaguides.springboot.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -24,5 +28,34 @@ public class PostController {
         List<PostDto> posts = postService.findAllPosts();
         model.addAttribute("posts", posts);
         return "/admin/posts";
+    }
+
+    // handler method to handle new post request
+    @GetMapping("/admin/posts/newpost")
+    public String newPostForm(Model model) {
+        PostDto postDto = new PostDto();
+        model.addAttribute("post", postDto);
+        return "/admin/create_post";
+    }
+
+    // handler method to handle form submit request
+    @PostMapping("/admin/posts")
+    public String createPost(@Valid @ModelAttribute("post") PostDto postDto, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("post", postDto);
+            return "/admin/create_post";
+        }
+        postDto.setUrl(getUrl(postDto.getTitle()));
+        postService.createPost(postDto);
+        return "redirect:/admin/posts";
+    }
+
+    private static String getUrl(String postTitle){
+        // OOPS Concepts Explained in Java
+        // oops-concepts-explained-in-java
+        String title = postTitle.trim().toLowerCase();
+        String url = title.replaceAll("\\s+", "-");
+        url = url.replaceAll("[^A-Za-z0-9]", "-");
+        return url;
     }
 }
--- springboot-blog-webapp/src/main/java/net/javaguides/springboot/service/impl/PostServiceImpl.java
+++ springboot-blog-webapp/src/main/java/net/javaguides/springboot/service/impl/PostServiceImpl.java
@@ -25,4 +25,10 @@ public class PostServiceImpl implements PostService {
         List<Post> posts = postRepository.findAll();
         return posts.stream().map(PostMapper::mapToPostDto).collect(Collectors.toList());
     }
+
+    @Override
+    public void createPost(PostDto postDto) {
+        Post post = PostMapper.mapToPost(postDto);
+        postRepository.save(post);
+    }
 }
